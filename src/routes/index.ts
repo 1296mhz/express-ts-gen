f9d65bbd897e0c1ab1604292c50b98cec1aeb328
Dmitry Zyryanov <r8cdf@yandex.ru>
2023-02-06 20:31:23 +0500

    Login page

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,12 +1,11 @@
 import express from 'express';
-// import token from 'policies/token';
 import root from './root';
 import register from './register';
-
+import login from './login';
 
 const router = express.Router();
 router.use('/', root);
 router.use('/register', register);
-// router.use(orders);
+router.use('/login', login);
 
 export default router;
